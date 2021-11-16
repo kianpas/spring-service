a5054f75bfa1729acd88605619536e3a61dc5000
kianpas <kianpas@gmail.com>
2021-11-17 00:55:46 +0900

    2021.11.17 테스트를 위한 테스트 클래스

---
+++ src/main/java/com/kianpas/book/springservice/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kianpas.book.springservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/test/java/com/kianpas/book/springservice/web/HelloControllerTest.java
+++ src/test/java/com/kianpas/book/springservice/web/HelloControllerTest.java
@@ -1,26 +1,31 @@
 package com.kianpas.book.springservice.web;
 
+import com.kianpas.book.springservice.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloReturn() throws Exception {
         String hello = "hello";
         mvc.perform(get("/hello"))
@@ -29,6 +34,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDtoReturn() throws Exception {
         String name = "hello";
         int amount = 1000;
